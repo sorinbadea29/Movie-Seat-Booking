80627371d4dd46dca818df12dfe779fe459957e5
Sorin Badea <62335902+sorinbadea29@users.noreply.github.com>
2020-07-06 13:20:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+const container = document.querySelector(".container");
+const movieSelect = document.getElementById("movie");
+const seats = document.querySelectorAll(".row .seat:not(.occupied) ");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+let ticketPrice = +movieSelect.value;
+
+populateUI();
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected ");
+  count.innerHTML = selectedSeats.length;
+  total.innerHTML = ticketPrice * selectedSeats.length;
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+}
+
+// Get data from localStorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+// Movie select event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener("click", (e) => {
+  if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")){
+    e.target.classList.toggle("selected");
+    updateSelectedCount();
+  }
+});
+
+// Inital count and total set
+updateSelectedCount();
